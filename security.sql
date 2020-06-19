855998421a39e2de31fb1f58babeaa1a3ccc80be
pubuduudara <pubuduudara7@gmail.com>
2020-06-19 21:05:44 +0530

    Security

--- security.sql
+++ security.sql
@@ -1,21 +1,63 @@
 -- Security has to be done as follows
 -- -------------------------------------------
 
-CREATE USER 'user_dealer'@'localhost' IDENTIFIED BY '123';
-ALTER USER 'user_dealer'@'localhost' IDENTIFIED BY '123';
+/*
+There are 4 user defined database roles from in the backend. Their jobs are as follows
 
-CREATE USER 'user_customer'@'localhost' IDENTIFIED BY '123';
-ALTER USER 'user_customer'@'localhost' IDENTIFIED BY '123';
+admin_dealer: Handle delear related table. admin_dealer sas full access to the stocks table. He can read and update Dealers, Dealer_members table.
+The reason for that is inorder to delete or insert new dealer or dealer memeber a higher level of access is required.
 
-CREATE USER 'user_delivery'@'localhost' IDENTIFIED BY '123';
-ALTER USER 'user_delivery'@'localhost' IDENTIFIED BY '123';
+admin_customer: admin_customer can read, update customers table
+
+admin_delivery: admin_delivery can read, update delivery_people table
+
+admin_orders: Responsible for read, write, delete, insert orders and manage delevieris.
+
+*/
+
+CREATE USER 'admin_dealer'@'localhost' IDENTIFIED BY '123';
+ALTER USER 'admin_dealer'@'localhost' IDENTIFIED BY '123';
+
+CREATE USER 'admin_customer'@'localhost' IDENTIFIED BY '123';
+ALTER USER 'admin_customer'@'localhost' IDENTIFIED BY '123';
+
+CREATE USER 'admin_delivery'@'localhost' IDENTIFIED BY '123';
+ALTER USER 'admin_delivery'@'localhost' IDENTIFIED BY '123';
+
+CREATE USER 'admin_orders'@'localhost' IDENTIFIED BY '123';
+ALTER USER 'admin_orders'@'localhost' IDENTIFIED BY '123';
+
+
+GRANT ALL ON 
+medical_db1.stocks TO 'admin_dealer'@'localhost';
+
+GRANT SELECT, UPDATE ON
+medical_db1.dealer_members TO 'admin_dealer'@'localhost';
+
+GRANT SELECT, UPDATE ON
+medical_db1.dealers TO 'admin_dealer'@'localhost';
+
+GRANT SELECT, UPDATE ON
+medical_db1.customers TO 'admin_customer'@'localhost';
+
+GRANT SELECT, UPDATE ON 
+medical_db1.delivery_people TO 'admin_delivery'@'localhost';
+
+GRANT ALL ON 
+medical_db1.deliveries TO 'admin_orders'@'localhost';
+
+GRANT ALL ON 
+medical_db1.order_information TO 'admin_orders'@'localhost';
+
+GRANT ALL ON 
+medical_db1.order_medicines TO 'admin_orders'@'localhost';
+
+
 
 -- According to dealer_members perspective
 	-- dealers has 
 		-- full access to dealers table, stocks table.(they can be CRUD(=Create,Read,...etc))
         -- can CRUD deliveries
-
-GRANT ALL PRIVILEGES ON database_name.* TO 'username'@'localhost';
 
 	-- delivery_people can't 
 		-- access 'stocks' table
